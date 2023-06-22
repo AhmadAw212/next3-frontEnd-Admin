13843d1d7996fc86835b3d7e08ec90676316505b
mustapha969960 <mustaphammohamad@gmail.com>
2023-06-22 15:00:55 +0300

    update

--- src/app/components/view-policy-dialog/view-policy-dialog.component.ts
+++ src/app/components/view-policy-dialog/view-policy-dialog.component.ts
@@ -2,6 +2,7 @@ import { DialogRef } from '@angular/cdk/dialog';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { DataServiceService } from 'src/app/services/data-service.service';
+import { DicoServiceService } from 'src/app/services/dico-service.service';
 
 @Component({
   selector: 'app-view-policy-dialog',
@@ -9,16 +10,24 @@ import { DataServiceService } from 'src/app/services/data-service.service';
   styleUrls: ['./view-policy-dialog.component.css'],
 })
 export class ViewPolicyDialogComponent implements OnInit {
+  dico?: any;
   constructor(
     private dataService: DataServiceService,
-    private dialogRef: MatDialogRef<ViewPolicyDialogComponent>
+    private dialogRef: MatDialogRef<ViewPolicyDialogComponent>,
+    private dicoService: DicoServiceService
   ) {}
   policyData?: any;
   policyCoverlist?: any;
   ngOnInit(): void {
     this.viewPolicy();
+    this.getDico();
   }
-
+  getDico() {
+    this.dicoService.getDico();
+    this.dicoService.dico.subscribe((data) => {
+      this.dico = data;
+    });
+  }
   viewPolicy() {
     this.dataService.viewPolicy('10.1.MO.347922.0.0.1').subscribe({
       next: (data) => {
