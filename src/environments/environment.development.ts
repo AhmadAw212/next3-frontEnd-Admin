d61794a3f5e3372d2bcf27e1a871ea661eeb5dec
AhmadAw212 <awadaahmad606@gmail.com>
2023-08-05 10:11:18 +0300

    update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-   userUrl: 'http://10.1.208.185:9090/next2/api',
+  //  userUrl: 'http://10.1.208.185:9090/next2/api',
   //userUrl: 'http://192.168.137.87:9090/next2/api',
-  //userUrl: 'http://localhost:9090/next2/api',
+  userUrl: 'http://localhost:9090/next2/api',
 };
-
