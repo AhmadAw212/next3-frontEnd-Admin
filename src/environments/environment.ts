d39fbd8bb997a70186af89e3b3038228c1ecb076
AhmadAw212 <awadaahmad606@gmail.com>
2024-01-02 11:36:54 +0200

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   // userUrl: 'http://10.1.208.185:9090/next2/api',
   // userUrl: 'http://192.168.137.87:9090/next2/api',
-  // loginUrl: 'http://192.168.137.87:9090/next2/api',
   userUrl: 'http://localhost:9090/next2/api',
   loginUrl: 'http://localhost:8080/next2/api',
   // userUrl: 'http://LS-PRDNEXT2DB.libano-suisse.com:9090/next2/api',
